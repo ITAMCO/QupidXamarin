479a352ea37fe0efc9cef1ccbe7bb14afb322589
Daniel Johnson <41651150+dpjohnson0010@users.noreply.github.com>
2020-04-07 14:39:00 -0400

    lotsa updatus

--- QupidMobile/QupidMobile/QupidMobile/MainPage.xaml.cs
+++ QupidMobile/QupidMobile/QupidMobile/MainPage.xaml.cs
@@ -5,6 +5,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Net;
+using System.IO;
+using Xamarin.Essentials;
 
 namespace QupidMobile
 {
@@ -15,7 +22,58 @@ namespace QupidMobile
     {
         public MainPage()
         {
+            checkifLoggedIn();
             InitializeComponent();
+        }
+        private async void checkifLoggedIn()
+        {
+            var userID = await SecureStorage.GetAsync("user_id");
+            if (userID != "" && userID != null)
+            {
+                Navigation.PushModalAsync(new transactions());
+            }
+        }
+        private void Login_Clicked(object sender, EventArgs e)
+        {
+            string result;
+            loginInfo pass = new loginInfo();
+            pass.username = UserName.Text;
+            pass.password = Password.Text;
+            var request = HttpWebRequest.Create("http://vsv-qupidweb-01:8410/api/values/");
+            request.ContentType = "application/json";
+            request.Method = "POST";
+            using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+            {
+                string json = JsonConvert.SerializeObject(pass, Formatting.Indented);
+
+                streamWriter.Write(json);
+            }
+            var httpResponse = (HttpWebResponse)request.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                result = streamReader.ReadToEnd();
+                
+            }
+            if (result == "0")
+                DisplayAlert("Login Incorrect", "Please enter a correct username and password", "OK");
+            else
+            {
+
+                JArray a = JArray.Parse(result);
+                JObject o = JObject.Parse(a[0].ToString());
+                userInformation.eid = o["Eid"].ToString();
+                
+                userInformation.fullName = o["FullName"].ToString();
+                SecureStorage.SetAsync("user_id", o["FullName"].ToString());
+                Application.Current.SavePropertiesAsync();
+                Navigation.PushModalAsync(new transactions());
+            }
+
+        }
+        public class loginInfo
+        {
+            public  string username { get; set; }
+            public  string password { get; set; }
         }
     }
 }
