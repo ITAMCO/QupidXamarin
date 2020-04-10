862774739e039c151e5eb316c95ffa787c961c7e
Steven Peregrine <38218791+Speregrine@users.noreply.github.com>
2020-04-10 16:14:24 -0400

    upload new project

---
+++ QupidMobile/QupidMobile/QupidMobile/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+namespace QupidMobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            AppCenter.Start("ios=38e4fd9c-bb59-40b2-978c-4c590ef04c47;android=bdf0bbb8-320e-4b44-8b9e-b8fdf78f459c;uwp=40f0adac-7eea-4689-bbfc-38ff2e259fdf", typeof(Analytics), typeof(Crashes));
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
