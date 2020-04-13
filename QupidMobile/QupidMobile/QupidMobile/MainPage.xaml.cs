97ce82adf7157ec9e59a573193ac351acfca92e2
Daniel Johnson <41651150+dpjohnson0010@users.noreply.github.com>
2020-04-13 15:35:09 -0400

    copying epix codes

--- QupidMobile/QupidMobile/QupidMobile/MainPage.xaml.cs
+++ QupidMobile/QupidMobile/QupidMobile/MainPage.xaml.cs
@@ -6,6 +6,12 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using Microsoft.AppCenter.Crashes;
+using System.Net;
+using Xamarin.Essentials;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
 namespace QupidMobile
 {
     // Learn more about making custom code visible in the Xamarin.Forms previewer
@@ -15,8 +21,60 @@ namespace QupidMobile
     {
         public MainPage()
         {
+            //checkifLoggedIn();
             InitializeComponent();
+            DisplayAlert("Qupid", "Did you want to restart Qupid so the designer works?", "yeet");
         }
-      
+        private async void checkifLoggedIn()
+        {
+            var userID = await SecureStorage.GetAsync("user_id");
+            if (userID != "" && userID != null)
+            {
+                Navigation.PushModalAsync(new transactions());
+            }
+        }
+        private void Login_Clicked(object sender, EventArgs e)
+        {
+            string result;
+            loginInfo pass = new loginInfo();
+            pass.username = UserName.Text;
+            pass.password = Password.Text;
+            var request = HttpWebRequest.Create("http://vsv-qupidweb-01:8410/api/values/");
+            request.ContentType = "application/json";
+            request.Method = "POST";
+            using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+            {
+                string json = JsonConvert.SerializeObject(pass, Formatting.Indented);
+
+                streamWriter.Write(json);
+            }
+            var httpResponse = (HttpWebResponse)request.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                result = streamReader.ReadToEnd();
+
+            }
+            if (result == "0")
+                DisplayAlert("Login Incorrect", "Please enter a correct username and password", "OK");
+            else
+            {
+
+                JArray a = JArray.Parse(result);
+                JObject o = JObject.Parse(a[0].ToString());
+                user.eid = o["Eid"].ToString();
+
+                user.fullName = o["FullName"].ToString();
+                SecureStorage.SetAsync("user_id", o["FullName"].ToString());
+                Application.Current.SavePropertiesAsync();
+                Navigation.PushModalAsync(new transactions());
+            }
+
+        }
+        public class loginInfo
+        {
+            public string username { get; set; }
+            public string password { get; set; }
+        }
+
     }
 }
